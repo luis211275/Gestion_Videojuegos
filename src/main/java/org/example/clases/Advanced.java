585f1d379dcc701763562cdbc64619c1122d290b
luis211275 <luislopeznunosanchez@gmail.com>
2026-04-15 11:04:10 +0200

    AÃñadido nuevos comentarios

--- src/main/java/org/example/clases/Advanced.java
+++ src/main/java/org/example/clases/Advanced.java
@@ -2,6 +2,9 @@ package org.example.clases;
 
 import java.util.Arrays;
 
+/**
+ * Clase advanced que extiende de suscripcion
+ */
 public class Advanced extends Suscripcion{
     public Advanced() {
         super("Advanced", 150, 2, Arrays.asList("FIFA", "MarioBros", "COD", "Zelda"));
